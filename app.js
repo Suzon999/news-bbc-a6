f78f313742b9055cd4b82b7d19bb2abe1d6db7a8
Suzon999 <ashikurrahamansuzon346112.com>
2022-09-02 22:48:50 +0600

    load catagory

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const loadCatagory = () => {
     fetch('https://openapi.programming-hero.com/api/news/categories')
         .then(res => res.json())
-        .then(data => displayCatagory(data.data))
+        .then(data => displayCatagory(data.data.news_category))
         .catch(error => displayCatagory(error))
 }
 
 const displayCatagory = (catagorys) => {
     const catagorysItems = document.getElementById("load-catagorious");
-    catagorysItems.classList.add('flex-items')
-    catagorysItems.innerText = catagorys
+    for (const catagory of catagorys) {
+        console.log(catagory)
+        const li = document.createElement("li");
+        li.classList.add('flex-items')
+        li.innerHTML = `<a onclick= " loadCatagoryDetails(${catagory.category_id} ) " href="">${catagory.category_name} </a>`
+        catagorysItems.appendChild(li)
+    }
 }
+
+
 loadCatagory();
